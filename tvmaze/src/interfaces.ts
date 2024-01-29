8bec8107e5ee18089988ae372332bd6a77678e37
Nathan Rydel <nathanrydel@gmail.com>
2024-01-29 15:01:29 -0700

    ran autoformatter

--- tvmaze/src/interfaces.ts
+++ tvmaze/src/interfaces.ts
@@ -1,25 +1,27 @@
 //{id, name, summary, image}
 interface IShow {
-  id: number,
-  name: string,
-  summary: string,
-  image: string
+  id: number;
+  name: string;
+  summary: string;
+  image: string;
 }
 
-//{id, name, summary, image}
-interface IShowResult {show: {
-  id: number,
-  name: string,
-  summary: string,
-  image: {medium?: string}
-}}
+//{ show: {id, name, summary, image} }
+interface IShowResult {
+  show: {
+    id: number;
+    name: string;
+    summary: string;
+    image: { medium?: string; };
+  };
+}
 
 //{ id, name, season, number }
 interface IEpisode {
   id: number,
   name: string,
   season: number,
-  number: number
+  number: number;
 }
 
-export type {IShow, IShowResult, IEpisode};
+export type { IShow, IShowResult, IEpisode };
